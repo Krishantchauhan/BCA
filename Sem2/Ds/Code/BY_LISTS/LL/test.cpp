8ef0289466db46c569ecaf7bf5a221e5d2c7800a
Krishantchauhan <krashantchauhan@gmail.com>
2023-02-04 19:45:38 +0530

    prev

---
+++ Sem2/Ds/Code/BY_LISTS/LL/test.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void disp(node* head){
+    while(head!= NULL)
+    {
+        printf(" %d \t",head->data);
+        head=head->next;
+    }
+    printf("\n");
+}
+
+void insertbeg(node* &head,int x){
+    node* newnode=new node(x);
+    if(head==NULL)
+        head=newnode;
+    else{
+    newnode->next=head;
+    head = newnode;
+    }
+}
+
+void end(node* &head,int x){
+    node* newnode = new node(x); 
+    node* curr = head;
+    if(curr==NULL)
+        head=newnode;
+    else{
+    while(curr->next!=NULL)
+        curr=curr->next;
+    curr->next=newnode;
+}}
+
+int size(node *head)
+{
+    int count=0;
+    while(head!=NULL)
+    {
+        head=head->next;
+        count++;
+    }
+    printf("\ntotal NUMBER of NOdes :- %d \n",count);
+    return count;
+}
+
+void delbeg(node* &head){
+    if(head == NULL)
+        printf("Empty LL");
+    else {
+    node* curr;
+    curr = head;
+    head=curr->next;
+    free(curr);
+    }
+}
+
+void delend(node* &head){
+    node* curr=head;
+    if(head == NULL)
+        printf("Empty LL");
+    else
+    {
+    while(curr->next->next!=NULL)
+        curr=curr->next;
+    free(curr->next);
+    curr->next=NULL;
+    }
+}
+
+void insertpos(node* &head,int pos,int x,int count){
+    node* newnode = new node(x);
+    node* curr = head;
+    if(pos==1)
+        insertbeg(head,x);
+    int i=1;
+    curr = head;
+    if(pos > count)
+        printf("InValid Position\n");
+    else{
+    while (i < pos - 1)
+    {
+      curr = curr->next;
+      i++;
+    }
+    newnode->next = curr->next;
+    curr->next = newnode;
+    printf("Successfull Incerted at %dth Position\n", pos);
+    }    
+}
+
+int main() {
+	// your code goes here
+	node* head=NULL;
+	head = new node(10);
+	head->next = new node(20);
+	head->next->next = new node(30);
+	
+// // 	disp(head);
+
+	// insertbeg(head,5);
+	// disp(head);
+
+	// end(head,15);
+	// disp(head);
+// // 	size(head);
+	
+	// delbeg(head);
+	// disp(head);
+	
+	// delend(head);
+	disp(head);
+	
+// // 	size(head);
+    int s;
+    s=size(head);
+    
+    insertpos(head,3,22,s);
+    disp(head);
+	
+	return 0;
+}
